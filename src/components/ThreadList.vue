c2e0f9c8cab5a836ef6d79464f92bf82fb288371
nikolajjsj <10490273+nikolajjsj@users.noreply.github.com>
2021-05-14 11:35:04 +0200

    Updated ThreadList component

--- src/components/ThreadList.vue
+++ src/components/ThreadList.vue
@@ -3,157 +3,62 @@
     <div class="thread-list">
       <h2 class="list-title">Threads</h2>
 
-      <div class="thread">
+      <div
+        v-for="thread in threads"
+        :key="thread.id"
+        class="thread">
         <div>
           <p>
-            <a href="thread.html">How can I chop onions without crying?</a>
+            <a href="#">{{ thread.title }}</a>
           </p>
           <p class="text-faded text-xsmall">
-            By <a href="profile.html">Joseph Kerr</a>, yesterday.
+            By <a href="#">{{ userById(thread.userId).name }}</a>, {{ thread.publishedAt }}.
           </p>
         </div>
 
         <div class="activity">
           <p class="replies-count">
-            1 reply
+            {{ thread.posts.length }} replies
           </p>
 
           <img
             class="avatar-medium"
-            src="http://i0.kym-cdn.com/photos/images/facebook/000/010/934/46623-batman_pikachu_super.png"
+            :src="userById(thread.userId).avatar"
             alt=""
           />
 
           <div>
             <p class="text-xsmall">
-              <a href="profile.html">Bruce Wayne</a>
+              <a href="#">{{ userById(thread.userId).name }}</a>
             </p>
-            <p class="text-xsmall text-faded">2 hours ago</p>
+            <p class="text-xsmall text-faded">{{ thread.publishedAt }}</p>
           </div>
         </div>
       </div>
-
-      <div class="thread">
-        <div>
-          <p>
-            <a href="thread.html">Wasabi vs horseraddish?</a>
-          </p>
-          <p class="text-faded text-xsmall">
-            By <a href="profile.html">Robin</a>, 8 hours ago
-          </p>
-        </div>
-
-        <div class="activity">
-          <p class="replies-count">
-            3 replies
-          </p>
-
-          <img
-            class="avatar-medium"
-            src="https://firebasestorage.googleapis.com/v0/b/forum-2a982.appspot.com/o/images%2Favatars%2Fraynathan?alt=media&token=bd9a0f0e-60f2-4e60-b092-77d1ded50a7e"
-            alt=""
-          />
-          <span>
-            <a class="text-xsmall" href="profile.html">Ray-Nathan James</a>
-            <p class="text-faded text-xsmall">3 hours ago</p>
-          </span>
-        </div>
-      </div>
-
-      <div class="thread">
-        <div>
-          <p>
-            <a href="thread.html">Multifilling</a>
-          </p>
-          <p class="text-faded text-xsmall">
-            By <a href="profile.html">Ray-Nathan James</a>, 6 days ago
-          </p>
-        </div>
-
-        <div class="activity">
-          <p class="replies-count">
-            1 reply
-          </p>
-
-          <img
-            class="avatar-medium"
-            src="http://i0.kym-cdn.com/photos/images/facebook/000/010/934/46623-batman_pikachu_super.png"
-            alt=""
-          />
-          <span>
-            <a class="text-xsmall" href="profile.html">Bruce Wayne</a>
-            <p class="text-faded text-xsmall">6 days ago</p>
-          </span>
-        </div>
-      </div>
-
-      <div class="thread">
-        <div>
-          <p>
-            <a href="thread.html">Egg replacer for bread dough?</a>
-          </p>
-          <p class="text-faded text-xsmall">
-            By <a href="profile.html">Theodor Jackson</a>, 2 weeks ago
-          </p>
-        </div>
-
-        <div class="activity">
-          <p class="replies-count">
-            1 reply
-          </p>
-
-          <img
-            class="avatar-medium"
-            src="http://icons.iconarchive.com/icons/designbolts/free-male-avatars/128/Male-Avatar-icon.png"
-            alt=""
-          />
-          <span>
-            <a class="text-xsmall" href="profile.html">Theodor Jackson</a>
-            <p class="text-faded text-xsmall">2 weeks ago</p>
-          </span>
-        </div>
-      </div>
-
-      <div class="thread">
-        <div>
-          <p>
-            <a href="thread.html">Which is your favorite carbohydrate? 🤓</a>
-          </p>
-          <p class="text-faded text-xsmall">
-            By <a href="profile.html">Ray-Nathan James</a>, 1 month ago
-          </p>
-        </div>
-
-        <div class="activity">
-          <p class="replies-count">
-            0 replies
-          </p>
-
-          <img
-            class="avatar-medium"
-            src="http://i0.kym-cdn.com/photos/images/facebook/000/010/934/46623-batman_pikachu_super.png"
-            alt=""
-          />
-          <span>
-            <a class="text-xsmall" href="profile.html">Ray-Nathan James</a>
-            <p class="text-faded text-xsmall">1 month ago</p>
-          </span>
-        </div>
-      </div>
-    </div>
-
-    <div class="pagination">
-      <button class="btn-circle" disabled>
-        <i class="fa fa-angle-left"></i>
-      </button>
-      1 of 3
-      <button class="btn-circle"><i class="fa fa-angle-right"></i></button>
     </div>
   </div>
 </template>
 
 <script>
-export default {}
+import sourceData from '@/data.json'
+
+export default {
+  data () {
+    return {
+      threads: sourceData.threads,
+      posts: sourceData.posts,
+      users: sourceData.users
+    }
+  },
+  methods: {
+    postById (postId) {
+      return this.posts.find(p => p.id === postId)
+    },
+    userById (userId) {
+      return this.users.find(u => u.id === userId)
+    }
+  }
+}
 </script>
 
 <style>
