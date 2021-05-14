3d123c1077d55ffd54631b1419723d558e100663
nikolajjsj <10490273+nikolajjsj@users.noreply.github.com>
2021-05-14 11:24:37 +0200

    Used vue router view

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,13 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <div class="container">
-    <PageHome />
+    <router-view />
   </div>
 </template>
 
 <script>
-import PageHome from '@/components/PageHome.vue'
-
 export default {
-  name: 'App',
-  components: {
-    PageHome
-  }
+  name: 'App'
 }
 </script>
 
